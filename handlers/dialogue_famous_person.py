6fab8fa9fd8d6068d836b8fb96031e6b218f1f50
Alex <af88_88@mail.ru>
2026-05-24 14:35:06 +0300

    Add project famous person

--- handlers/dialogue_famous_person.py
+++ handlers/dialogue_famous_person.py
@@ -1,14 +1,51 @@
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram import F
-
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from aiogram import Router
+from services.chat_gpt import ChatGptService
 from aiogram.filters.command import Command
+from keyboards.keyboards import personalities_keyboard, finish_keyboard
+from data.personal_prompts import PERSON_PROMPTS
 
 
 router = Router()
 
 
-@router.message(F.text.contains('talk'))
-@router.message(F.text.contains("Диалог с известной личностью"))
-async def start(message: Message):
-    await message.answer('Test')
+class TalkState(StatesGroup):
+    choosing = State()
+    talking = State()
+
+@router.message(Command('talk'))
+@router.message(F.text & F.text.contains("Диалог с известной личностью"))
+async def talk_person(message: Message, state: FSMContext, chat_gpt_service: ChatGptService):
+    await state.set_state(TalkState.choosing)
+
+    prompt_text = 'Einstein'
+    image_person = await chat_gpt_service.generate_image(prompt_text)
+
+    await message.answer_photo(
+        image_person,
+        caption="Выбери личность, с которой хочешь поговорить:",
+        reply_markup=personalities_keyboard()
+    )
+
+@router.callback_query(F.data.in_(PERSON_PROMPTS.keys()))
+async def choose_personality(callback: CallbackQuery, state: FSMContext):
+    personality = callback.data
+    await state.update_data(prompt=PERSON_PROMPTS[personality])
+    await state.set_state(TalkState.talking)
+
+    await callback.message.answer(
+        f"Отлично! Теперь ты разговариваешь с *{callback.data}*.\nПиши сообщение.",
+        parse_mode="Markdown",
+        reply_markup=finish_keyboard()
+    )
+    await callback.answer()
+
+
+@router.callback_query(F.data == "finish")
+async def finish(callback: CallbackQuery, state: FSMContext):
+    await state.clear()
+    await callback.message.answer("Диалог завершён. Используй /talk чтобы начать снова.")
+    await callback.answer()
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,5 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 btn1 = KeyboardButton(text='/start')
 btn2 = KeyboardButton(text='Рандомный факт')
@@ -11,7 +10,6 @@ btn7 = KeyboardButton(text='/another_fact')
 btn8 = KeyboardButton(text='/exit')
 
 
-
 keyboards_1 = [
     [btn1, btn2, btn3],
     [btn4, btn5]
@@ -20,6 +18,20 @@ keyboards_2 = [
     [btn7, btn8],
 ]
 
+
 kb1 = ReplyKeyboardMarkup(keyboard=keyboards_1, resize_keyboard=True)
 kb2 = ReplyKeyboardMarkup(keyboard=keyboards_2, resize_keyboard=True)
 
+
+def personalities_keyboard():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Альберт Эйнштейн", callback_data="einstein")],
+        [InlineKeyboardButton(text="Стив Джобс", callback_data="jobs")],
+        [InlineKeyboardButton(text="Фрейд", callback_data="freud")],
+    ])
+
+
+def finish_keyboard():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Закончить", callback_data="finish")]
+    ])
